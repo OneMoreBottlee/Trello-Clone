79642b8265d7e066e769d720f1de073f43cd5ecc
OneMoreBottlee <hanbj0301@gmail.com>
2022-10-23 23:53:52 +0900

    7.11

--- src/Componants/Board.tsx
+++ src/Componants/Board.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components"
 import DraggableCard from "./DragabbleCard"
 
 const Wrapper = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+    padding: 20px 10px;
+    padding-top: 30px;
+    background-color: ${(props) => props.theme.boardColor};
+    border-radius: 5px;
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
 `
 
 const Title = styled.h2`
@@ -16,6 +18,17 @@ const Title = styled.h2`
     margin-bottom: 10px;
     font-size: 18px;
 `
+
+const Area = styled.div<IAreaProps>`
+    background-color: ${props => props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue"};
+    flex-grow: 1;
+    transition: background-color .3s ease-in-out;
+`
+
+interface IAreaProps{
+    isDraggingOver: boolean;
+    isDraggingFromThis: boolean;
+}
 
 interface IBoardProps {
     toDos: string[];
@@ -27,15 +40,20 @@ function Board({ toDos, boardId }: IBoardProps) {
     return (
         <Wrapper>
             <Title>{boardId}</Title>
-        <Droppable droppableId={boardId}>
-            {(magic) => (
-                <div ref={magic.innerRef} {...magic.droppableProps}>
-                    {toDos.map((toDo, index) => (
-                        <DraggableCard key={toDo} index={index} toDo={toDo}></DraggableCard>
-                    ))}
-                    {magic.placeholder}
-                </div>)}
-        </Droppable>
+            <Droppable droppableId={boardId}>
+                {(magic, info) => (
+                    <Area
+                        isDraggingOver={info.isDraggingOver}
+                        isDraggingFromThis={Boolean(info.draggingFromThisWith)}
+                        ref={magic.innerRef}
+                        {...magic.droppableProps}
+                    >
+                        {toDos.map((toDo, index) => (
+                            <DraggableCard key={toDo} index={index} toDo={toDo}></DraggableCard>
+                        ))}
+                        {magic.placeholder}
+                    </Area>)}
+            </Droppable>
         </Wrapper>
     )
 }
